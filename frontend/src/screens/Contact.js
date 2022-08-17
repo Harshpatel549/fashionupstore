f67c46eb449b29cc99a50349cf28ab9b3547bef7
Harsh <93149672+Harshpatel199@users.noreply.github.com>
2022-08-17 14:08:28 -0400

    Update Contact.js

--- frontend/src/screens/Contact.js
+++ frontend/src/screens/Contact.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser';
-import useAnalyticsEventTracker from './useAnalyticsEventTracker';
+import useAnalyticsEventTracker from '../components/useAnalyticsEventTracker';
 import '../styles/contact.css';
 
 
